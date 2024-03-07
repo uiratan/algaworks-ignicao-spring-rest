d72d77180f110cbc2b9297fa8b17e493039b7a7f
Uiratan Cavalcante <uiratan@gmail.com>
2024-03-07 18:42:26 -0300

    3.8. Injetando e usando o repositório do SDJ

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,25 +1,24 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
+import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
 
-	@PersistenceContext
-	private EntityManager manager;
+	private final ProprietarioRepository proprietarioRepository;
 
 	@GetMapping("/proprietarios")
 	public List<Proprietario> listar() {
-		TypedQuery<Proprietario> query = manager.createQuery("from Proprietario", Proprietario.class);
-		return query.getResultList();
+		return proprietarioRepository.findAll();
 	}
+	
 }
