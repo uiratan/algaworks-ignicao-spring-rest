c36607406ecbc97396079bfed9f5355f6e6bc473
Uiratan Cavalcante <uiratan@gmail.com>
2024-03-07 14:59:23 -0300

    3.5. Mapeando entidades com Jakarta Persistence

--- src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,15 +1,29 @@
 package com.algaworks.algatransito.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Proprietario {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String nome;
 	private String email;
+
+	@Column(name = "fone")
 	private String telefone;
 
 }
